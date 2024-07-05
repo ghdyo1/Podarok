b889ae2375bf24131d6a10add7639f5d8297c78c
ghdyo1 <ghdyo999@gmail.com>
2024-07-05 14:55:41 +0300

    commit

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 let starth1 = document.querySelector("#hello");
 let startp = document.querySelector("#summon");
 let button = document.querySelector("#button");
-let btntexts = ["Я кнопка", "Я помогу тебе разукрасить сайт", "Для начала нужно выбрать фон", "BACKGROUND"];
+let btntexts = ["Я кнопка", "Я помогу тебе разукрасить сайт", "Для начала нужно выбрать фон", "Отправить"];
 let index = 0;
+let quiz = document.querySelector("#quiz");
 
 function start(event){
     starth1.style.display = "none";
     startp.style.display = "none";
     let key = event.key;
     if(key == "s"){
-        button.style.display = "inline-block";
+        button.style.display = "block";
     }
 }
 function buttonf(){
+    if(btntexts[index] == "Отправить"){
+        quiz.style.display = "inline-block";
+    }
     button.innerHTML = btntexts[index];
     index++;
 }
