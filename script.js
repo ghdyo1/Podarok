12f5b9470ffe6520d5c3700789467f1d506b88df
ghdyo1 <ghdyo999@gmail.com>
2024-07-05 07:15:36 +0300

    radio

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+let starth1 = document.querySelector("#hello");
+let startp = document.querySelector("#summon");
 let button = document.querySelector("#button");
-let btntexts = []
-let index = 0
+let btntexts = ["Я кнопка", "Я помогу тебе разукрасить сайт", "Для начала нужно выбрать фон", "BACKGROUND"];
+let index = 0;
 
 function start(event){
+    starth1.style.display = "none";
+    startp.style.display = "none";
     let key = event.key;
     if(key == "s"){
         button.style.display = "inline-block";
     }
 }
-function button(){
+function buttonf(){
+    button.innerHTML = btntexts[index];
+    index++;
 }
 
 document.addEventListener("keydown", start);
+button.addEventListener("click", buttonf);
